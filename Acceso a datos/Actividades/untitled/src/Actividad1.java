b7c03fb9079d915185d0552b39ac6e572a25865e
Enrique <fersanzenrique@gmail.com>
2026-09-17 20:50:23 +0200

    Ejercicios de Acceso(Fix)

--- Acceso a datos/Actividades/untitled/src/Actividad1.java
+++ Acceso a datos/Actividades/untitled/src/Actividad1.java
@@ -6,28 +6,48 @@ import java.io.InputStreamReader;
 public class Actividad1
 {
     public static void main(String[] args) {
+        /*
+        Ponemos todo nuestro código en un try con recursos pillando las excepciones
+        que pueden saltar del BufferedReader y cerrándolo automáticamente.
+         */
         try (BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in)))
         {
+            /*
+            Preguntamos al usuario que ruta quiere poner, he puesto un "./" antes para hacer saber
+            que es la carpeta de este proyecto
+             */
             System.out.println("Pon la ruta relativa de lo que quieras listar:\n->./");
             String ruta = String.valueOf(teclado.readLine());
             File directorio = new File("./" + ruta);
             File[] lista = directorio.listFiles();
+            /*
+            Antes de comprobar nada, tendríamos que saber si la lista existe.
+             */
             if (lista != null)
             {
                 for (File archivo : lista)
                 {
                     System.out.printf("-> " + archivo + "\s");
+                    /*
+                    Un if y else if, ya que aunque archivo no sea un directorio, no lo convierte en un archivo.
+                     */
                     if (archivo.isDirectory())
                     {
-                        System.out.printf("es un directorio.\n");
+                        System.out.print("es un directorio.\n");
                     }
                     else if (archivo.isFile())
                     {
-                        System.out.printf("es un archivo.\n");
+                        System.out.print("es un archivo.\n");
                     }
                 }
             }
-        } catch (RuntimeException | IOException e) {
+            else
+            {
+                System.out.println("El directorio " + directorio.getAbsolutePath() + " no existe");
+            }
+        }
+        catch (RuntimeException | IOException e)
+        {
             throw new RuntimeException(e);
         }
     }
--- Acceso a datos/Actividades/untitled/src/Actividad2.java
+++ Acceso a datos/Actividades/untitled/src/Actividad2.java
@@ -6,31 +6,45 @@ public class Actividad2
 {
     public static void main(String[] args)
     {
+        /*
+        Cómo en el ejercicio anterior, todo está en el try
+         */
         try (BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in)))
         {
-            System.out.println("Pon la ruta relativa de lo que quieras renombrar:\n->./");
+            System.out.println("Pon la ruta relativa de lo que quieras renombrar o limpiar de etiquetas:\n->./");
             String ruta = String.valueOf(teclado.readLine());
             File directorio = new File("./" + ruta);
-            System.out.println(directorio.canRead());
             File[] lista = directorio.listFiles();
+            /* Hasta aquí la misma filosofía */
             if (lista != null)
             {
                 for (File archivo : lista)
                 {
                     String nuevoNombre = archivo.getAbsolutePath();
-                    System.out.println(archivo.canRead());
-                    for (int i = 0; i < 4; i++) {
-                        if (archivo.getAbsolutePath().substring(archivo.getAbsolutePath().length() - i).equals("_"))
+                    /*
+                     - ¿Por qué queremos la ruta absoluta?
+                     - Porque para luego poder manejar el renombrado (que se trata de crear otro archivo)
+                     tenemos que saber donde estaba en primer lugar, si solo trabajamos con el nombre del archivo
+                     la localización de este se pierde.
+                     */
+                    for (int i = 0; i < 5; i++) {
+                        if (archivo.getAbsolutePath().substring(archivo.getAbsolutePath().length() - i).contains("_"))
                         {
                             nuevoNombre = archivo.getAbsolutePath().substring(0, (archivo.getAbsolutePath().length() - i));
                         }
                     }
+                    /*
+                    - ¿Qué hace este bucle for?
+                    - Lo que hace es mirar el nombre del archivo o carpeta desde atrás, sí "i" contiene "_"
+                    el nombre del archivo desde el index, hacia adelante es borrado.
+                    El programa si ve que ya está escrito cualquier cosa que acabe con _ este borra lo que haya adelante.
+                    Es utíl para no acabar viendo ficheros con "..._RWX_RWX_RWX".
+                    Cada paso que da el bucle es el siguiente: X -> WX -> RWX -> _RWX.
+                    Por eso utilizé contains() en vez equals().
+                     */
                     archivo.renameTo(new File(nuevoNombre));
-                    System.out.println(nuevoNombre);
+                    // No tengo que usar el boolean que devuelve renameTo() en este caso
                     nuevoNombre += "_";
-                    // System.out.println(archivo.canRead());
-                    System.out.println(archivo.exists());
-
                     if (archivo.canRead())
                     {
                         nuevoNombre += "R";
@@ -43,9 +57,15 @@ public class Actividad2
                     {
                         nuevoNombre += "X";
                     }
+                    /*
+                    Cadena de if, ya que todos los casos pueden ser ciertos
+                     */
                     archivo.renameTo(new File(nuevoNombre));
-                    System.out.println(nuevoNombre);
                 }
+            }
+            else
+            {
+                System.out.println("Ruta no encontrada");
             }
         } catch (Exception e)
         {
